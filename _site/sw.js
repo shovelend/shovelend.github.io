0c94ac4daaa6fd227ae97430cad962ee937b080e
Attila Molnar <shovelend@gmail.com>
2018-06-14 23:11:46 +0100

    :fire: no code is good code

--- _site/sw.js
+++ _site/sw.js
@@ -2,9 +2,10 @@ importScripts('/cache-polyfill.js');
 
 self.addEventListener('install', function(e) {
  e.waitUntil(
-   caches.open('airhorner').then(function(cache) {
+   caches.open('shovelend-v2').then(function(cache) {
      return cache.addAll([
        '/',
+       '/manifest.json',
        '/index.html',
        '/css/styles.css',
        '/images/cater_me.png',
@@ -13,4 +14,36 @@ self.addEventListener('install', function(e) {
      ]);
    })
  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
 });
